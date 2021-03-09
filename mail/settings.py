1ce1680522569786ca2b0a2da43ce5ef340ec82c
Marwan Sa-ad <saadmarwan16@gmail.com>
2021-03-09 23:11:03 +0300

    Setting up heroku

--- mail/settings.py
+++ mail/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ['mail.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['mail2001.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
